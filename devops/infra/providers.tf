2095928e8f93865c3c41e3b08ed9a2f130b21998
prashant1599 <singhprashant2599@gmail.com>
2024-08-13 11:13:59 +0530

    update CI/CD

--- devops/infra/providers.tf
+++ devops/infra/providers.tf
@@ -5,14 +5,7 @@ terraform {
       version = "5.11.0"
     }
   }
-
-#   backend "s3" {
-#     bucket = "statebackendbackup"
-#     region = "ap-south-1"
-#     # key = "state/ecs-ecr-s3.tfstate" 
-#   }
-# }
-
+}
 provider "aws" {
   region = "ap-south-1"
 
