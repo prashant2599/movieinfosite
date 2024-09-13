5ea5a0ee8ca74aa692944ea1bdd1d17f50e7689a
prashant1599 <singhprashant2599@gmail.com>
2024-09-13 15:24:55 +0530

    aws-infra ci/cd

--- aws-infra/outputs.tf
+++ aws-infra/outputs.tf
@@ -1,7 +1,7 @@
-output "instance_id" {
-  value = aws_instance.myprojectinstance.id
-}
+# output "instance_id" {
+#   value = aws_instance.myprojectinstance.id
+# }
 
-output "instance_public_ip" {
-  value = aws_instance.myprojectinstance.instance_public_ip
-}
+# output "instance_public_ip" {
+#   value = aws_instance.myprojectinstance.instance_public_ip
+# }
